eedf460966bf99e37d7b99638c2282231c7acf41
MhRizki <mhrizki515@gmail.com>
2024-04-28 05:17:41 +0700

    Update all file tugas algortima

--- soal1.cpp
+++ soal1.cpp
@@ -4,29 +4,29 @@
 using namespace std;
 
 int main(){
-   //Soal nomor 1
-   //  string nama,tempatTinggal;
-   //  int umur,tabungan;
 
-   //  cout << "Masukkan Nama: ";
-   //  getline(cin, nama);
-   //  cout << "Masukkan Umur: ";
-   //  cin >> umur;
-   //  cout << "Masukkan Tempat Tinggal: ";
-   //  cin.ignore();
-   //  getline(cin, tempatTinggal);
-   //  cout << "Masukkan Uang Tabungan: ";
-   //  cin >> tabungan;
+    string nama,tempatTinggal;
+    int umur,tabungan;
 
-   //  if (umur > 40 && tempatTinggal == "Nevada" || tempatTinggal == "New York" || tempatTinggal == "Havana" && tabungan > 10000000)
-   //  {
-   //     cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Don";
-   //  } else if(umur >= 25 && umur <= 40 && tempatTinggal == "New Jersey" || tempatTinggal == "Manhattan" || tempatTinggal == "Nevada" && tabungan == 2000000){
-   //     cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Underbos";
-   //  } else if(umur >= 18 && umur <= 24 && tempatTinggal == "California" || tempatTinggal == "Detroit" || tempatTinggal == "Boston" && tabungan < 1000000){
-   //       cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Capo";
-   //  } else {
-   //       cout << nama << " " << "Tidak mencurigakan";
-   //  }
+    cout << "Masukkan Nama: ";
+    getline(cin, nama);
+    cout << "Masukkan Umur: ";
+    cin >> umur;
+    cout << "Masukkan Tempat Tinggal: ";
+    cin.ignore();
+    getline(cin, tempatTinggal);
+    cout << "Masukkan Uang Tabungan: ";
+    cin >> tabungan;
+
+    if (umur > 40 && tempatTinggal == "Nevada" || tempatTinggal == "New York" || tempatTinggal == "Havana" && tabungan > 10000000)
+    {
+       cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Don";
+    } else if(umur >= 25 && umur <= 40 && tempatTinggal == "New Jersey" || tempatTinggal == "Manhattan" || tempatTinggal == "Nevada" && tabungan == 2000000){
+       cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Underbos";
+    } else if(umur >= 18 && umur <= 24 && tempatTinggal == "California" || tempatTinggal == "Detroit" || tempatTinggal == "Boston" && tabungan < 1000000){
+         cout << nama << " " << "kemungkinan adalah seorang anggota mafia dengan pangkat Capo";
+    } else {
+         cout << nama << " " << "Tidak mencurigakan";
+    }
     return 0;
 }
